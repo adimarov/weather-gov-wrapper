99af4da38fe854d0178a90689fe6ada288d2fabf
adimarov <adimarov@jaggedpeak.com>
2023-11-19 23:25:10 -0500

    Initial commit

---
+++ Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using GeoJSON.Net.Feature;
+using Newtonsoft.Json;
+using Polly;
+using RestSharp;
+using WeatherGov.Wrapper.Api;
+using WeatherGov.Wrapper.Client;
+
+
+Random jitter = new Random();
+
+
+var policy = Policy<RestResponse>
+  .Handle<Exception>()
+  .WaitAndRetryAsync(5,
+      retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                    + TimeSpan.FromMilliseconds(jitter.Next(0, 1000))
+  );
+
+RetryConfiguration.AsyncRetryPolicy = policy;
+
+var api = new DefaultApi("https://api.weather.gov");
+var result = await api.GridpointForecastAsync(WeatherGov.Wrapper.Model.NWSForecastOfficeId.TBW, 61, 104);
---
+++ src/WeatherGov.Wrapper/Model/ForecastFeature.cs
@@ -0,0 +1,39 @@
+﻿using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherGov.Wrapper.Model
+{
+    public class ForecastFeature : Feature
+    {
+        public ForecastFeature(IGeometryObject geometry, IDictionary<string, object> properties = null, string id = null) : base(geometry, properties, id)
+        {
+        }
+
+        public List<GridpointForecastPeriod> Periods { get; set; }
+
+        public static ForecastFeature FromFeature(Feature feature)
+        {
+            ForecastFeature ffeature = new ForecastFeature(feature.Geometry, feature.Properties, feature.Id);
+            ffeature.Periods = new List<GridpointForecastPeriod>();
+
+            if (feature.Properties!= null && feature.Properties.ContainsKey("periods"))
+            {
+                var jarr = (JArray)feature.Properties["periods"];
+                foreach ( var j1 in jarr)
+                {
+                    ffeature.Periods.Add(JsonConvert.DeserializeObject<GridpointForecastPeriod>(JsonConvert.SerializeObject(j1)));
+                }
+                
+            }
+            return ffeature;
+        }
+    }
+}
